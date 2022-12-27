b4705acb2b3dba730bfb8bb460612a914e1aeeec
Ahmed Meskine <40129162+midoum@users.noreply.github.com>
2022-12-27 16:16:19 +0100

    Test API python
    -Generatiing Access Token
    -Converting doc to text and splitting it

--- polls/urls.py
+++ polls/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('enc_key', views.enc_key, name='enc_key'),
     path('access_token', views.access_token, name='access_token'),
+    path('verify_token',views.verify,name='verify_token'),
+    path('convert',views.convert,name='convert'),
+       path('split',views.split_text,name='split_text'),
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from cryptography.fernet import Fernet
 import json
+import docx2txt
+from datetime import datetime
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
+#Generate an encription Key 
 def enc_key(request):
     k = Fernet.generate_key()
     c={"key": k.decode() }
@@ -14,12 +17,65 @@ def enc_key(request):
 
     
     return HttpResponse(key)
+
+
+#Generate access token for requested key 
 def access_token(request):
-    
-    key=Fernet.generate_key()
+    now = datetime.now()
+    date=now.strftime("%d-%m-%Y-%H:%M:%S")
+    dt_string=now.strftime("%d%m%Y%H%M%S")
+    key=request.GET.get('key')
     ip=request.META.get('REMOTE_ADDR')
     fernet=Fernet(key)
-    acc_token=fernet.encrypt(ip.encode())
-    array={'Statut':'Success','access token':acc_token.decode(),}
+    enc_ip=fernet.encrypt(ip.encode())
+    enc_date=fernet.encrypt(dt_string.encode())
+    acc_token=enc_ip.decode()+" "+enc_date.decode()
+    array={'Statut':'Success','access_token':acc_token,'date':date,}
     res=json.dumps(array)
     return HttpResponse(res)
+
+
+#verify the ip adress of client and token validity 
+def verify(request):
+    now = datetime.now()
+    dt_string=now.strftime("%d%m%Y%H%M%S")
+    key=request.GET.get('key')
+    token=request.GET.get('token')
+    token_to_split=str(token).split(" ")
+    enc_ip=token_to_split[0]
+    enc_date=token_to_split[1]
+    fernet=Fernet(key)
+    client_ip=fernet.decrypt(enc_ip).decode()
+    client_issue_date=fernet.decrypt(enc_date).decode()
+    array={'ip':client_ip,'date_of_issue':client_issue_date}
+    return HttpResponse(json.dumps(array))
+
+# Conversion du fichier word en text brute
+def convert(request):
+    file=request.GET.get('file')
+    # Define Variable
+    text = docx2txt.process(file)
+    
+    
+    # Create Dictionary
+    value = {
+        "text": text
+        
+    }
+ 
+    # Dictionary to JSON Object using dumps() method
+    # Return JSON Object
+    return HttpResponse(json.dumps(value))
+
+#Segmentation du texte en ligne 
+def split_text(request):
+        text=request.GET.get('text')
+        lines=str(text).split('.')
+        value=[]
+        i=1
+        for line in lines :
+            value.append({i:line})
+            i+=1
+        return HttpResponse(json.dumps(value))
+
+ 
